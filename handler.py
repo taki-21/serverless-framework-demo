f54150f1bbd5682dd9e6e09f659b871ee0ba2b1b
taki-21 <taki.it.0208.21@gmail.com>
2022-07-19 22:32:26 +0900

    add: handlerの修正

--- handler.py
+++ handler.py
@@ -3,8 +3,15 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
+        "plan_size": {
+            "wight": 3309.0,
+            "height": 2339.0
+        }
     }
+
+    # body = {
+    #     "message": "Go Serverless v3.0! Your function executed successfully!",
+    #     "input": event,
+    # }
 
     return {"statusCode": 200, "body": json.dumps(body)}
